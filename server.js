7603321d83a77cad0348043260313340e2181441
haofei <2361503654@qq.com>
2020-12-20 02:56:58 +0800

    service

---
+++ server.js
@@ -0,0 +1,6 @@
+var http=require('http');
+http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.end('Hello,NodeJs World!\n');
+}).listen(9000);
+console.log('Serve running at http://127.0.0.1:9000/');
